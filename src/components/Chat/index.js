93ee290808b2ffe94e386d0e3cdc70db84d2a79e
freshcodeAlice <freshjs.ofte@gmail.com>
2023-05-08 11:51:57 +0300

    ref and scrolling done

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import styles from './Chat.module.css';
 import ChatItem from './ChatItem';
 
 const Chat = (props) => {
+  const chatEndPoint = useRef(null);
+
+  const scrollIntoEndPoint = () => {
+    chatEndPoint.current.scrollIntoView({
+      behavior: "smooth"
+    })
+  }
+
+  useEffect(() => {
+    scrollIntoEndPoint();
+  }, [props.messages]);
+
   const {messages} = props;
-  const mapToChat = messages.map(m => <ChatItem message={m}/>) 
+  const mapToChat = messages.map(m => <ChatItem message={m} key={m.id}/>) 
     return (
         <div className={styles['chat']}>
           {mapToChat}
+          <div ref={chatEndPoint}></div>
         </div>
     );
 }
